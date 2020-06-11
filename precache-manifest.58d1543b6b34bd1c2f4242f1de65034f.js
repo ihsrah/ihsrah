9bf07e9493760f6385d931417f2782ab23718a9b
9antriksh <9antriksh@gmail.com>
2020-06-12 02:19:57 +0530

    Updates

---
+++ precache-manifest.58d1543b6b34bd1c2f4242f1de65034f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac3849e76e2c2384e826585ada2ad3f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a113c53fceb27b426dea",
+    "url": "/static/css/main.0322f164.chunk.css"
+  },
+  {
+    "revision": "59238ec9da235b71abd7",
+    "url": "/static/js/2.49304e1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.49304e1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a113c53fceb27b426dea",
+    "url": "/static/js/main.9c35e026.chunk.js"
+  },
+  {
+    "revision": "65eff825f8e43c8450bf",
+    "url": "/static/js/runtime-main.34327c3c.js"
+  },
+  {
+    "revision": "fb3e8918e970e46f493a825ab84864eb",
+    "url": "/static/media/Winterlady.fb3e8918.ttf"
+  }
+]);
