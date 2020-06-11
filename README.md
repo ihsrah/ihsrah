1c3d48d8951f3ef06f114726a007a0402b9cde3c
9antriksh <9antriksh@gmail.com>
2020-06-12 01:30:16 +0530

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-## ihsrah website
---
+++ precache-manifest.058d8536c8f052c657206f009680c1f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e082a24172b6f0ef12c89c425d3b165",
+    "url": "/ihsrah/index.html"
+  },
+  {
+    "revision": "45ec06d23f22f218b90d",
+    "url": "/ihsrah/static/css/main.1e1e8b78.chunk.css"
+  },
+  {
+    "revision": "59238ec9da235b71abd7",
+    "url": "/ihsrah/static/js/2.49304e1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ihsrah/static/js/2.49304e1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45ec06d23f22f218b90d",
+    "url": "/ihsrah/static/js/main.b668e5e9.chunk.js"
+  },
+  {
+    "revision": "74faec88539f4d013e59",
+    "url": "/ihsrah/static/js/runtime-main.4ebb284d.js"
+  },
+  {
+    "revision": "fb3e8918e970e46f493a825ab84864eb",
+    "url": "/ihsrah/static/media/Winterlady.fb3e8918.ttf"
+  }
+]);
