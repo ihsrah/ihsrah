1ae8f52e4aa7e1dd622f7888511b88014cce8417
9antriksh <9antriksh@gmail.com>
2020-06-12 21:32:19 +0530

    Added social links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,78 @@
 import React from "react";
 import "./App.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faInstagram,
+  faFacebook,
+  faTwitter,
+  faPinterest,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+library.add(faInstagram, faFacebook, faTwitter, faPinterest);
 
 function App() {
   return (
     <div className="App">
       <p className="Name">ihsrah</p>
+      <section className="Social">
+        <ul className="List">
+          <li>
+            <a
+              href="https://instagram.com/ihsrah.illustrations"
+              target="_blank"
+            >
+              <FontAwesomeIcon
+                icon={["fab", "instagram"]}
+                // mask={["fas", "circle"]}
+                // transform="grow-7 left-1.5 up-2.2"
+                // fixedWidth
+                color="white"
+                size="2x"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.facebook.com/Ihsrahart-113107960436454"
+              target="_blank"
+            >
+              <FontAwesomeIcon
+                icon={["fab", "facebook"]}
+                // mask={["fas", "circle"]}
+                // transform="grow-7 left-1.5 up-2.2"
+                // fixedWidth
+                color="white"
+                size="2x"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/ihsrah_art" target="_blank">
+              <FontAwesomeIcon
+                icon={["fab", "twitter"]}
+                // mask={["fas", "circle"]}
+                // transform="grow-7 left-1.5 up-2.2"
+                // fixedWidth
+                color="white"
+                size="2x"
+              />
+            </a>
+          </li>
+          {/* <li>
+            <a href="google.com" target="_blank">
+              <FontAwesomeIcon
+                icon={["fab", "pinterest"]}
+                // mask={["fas", "circle"]}
+                // transform="grow-7 left-1.5 up-2.2"
+                // fixedWidth
+                color="white"
+                size="2x"
+              />
+            </a>
+          </li> */}
+        </ul>
+      </section>
     </div>
   );
 }
